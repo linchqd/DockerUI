bcdf8a4d26a8aa6d68cebfb429c09e7eaf5ca32a
linchqd <linchqd@163.com>
2019-12-13 17:30:39 +0800

    webshell

--- src/views/Console.vue
+++ src/views/Console.vue
@@ -4,36 +4,52 @@
 <script>
 import 'xterm/css/xterm.css'
 import { Terminal } from 'xterm'
-import { FitAddon } from 'xterm-addon-fit'
+// import { FitAddon } from 'xterm-addon-fit'
 import { AttachAddon } from 'xterm-addon-attach'
 
 export default {
   data () {
     return {
-      copy: ''
+      data: { 'token': '123456' },
+      copy: '',
+      terminal: null,
+      webSocket: null
+    }
+  },
+  methods: {
+    socketConnected (event) {
+      this.webSocket.send(JSON.stringify(this.data))
+    },
+    socketClosed (event) {
+      this.terminal.write('\n\r\x1B[1;3;31msocket is already closed.\x1B[0m')
+    },
+    socketError (event) {
+      console.log('Error: ' + event)
+    },
+    socketMessage (event) {
+      this.terminal.write(event.data)
     }
   },
   mounted () {
-    let terminalContainer = document.getElementById('terminal')
-    let terminal = new Terminal({
-      cursorBlink: true
+    this.terminal = new Terminal({
+      cols: 92,
+      rows: 22,
+      cursorBlink: true,
+      cursorStyle: 'underline',
+      scrollback: 800,
+      tabStopWidth: 8,
+      screenKeys: true
     })
-    const fitAddon = new FitAddon()
-    terminal.loadAddon(fitAddon)
-    terminal.open(terminalContainer, true)
-    terminal.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
-    fitAddon.fit()
-
-    let webSocket = new WebSocket('ws:**********')
-    webSocket.binaryType = 'arraybuffer'
-    webSocket.onopen = function (evt) {
-      console.log('onopen', evt)
-      terminal.writeln(
-        '******************************************************************'
-      )
-    }
-    const attachAddon = new AttachAddon(webSocket)
-    terminal.loadAddon(attachAddon)
+    // const fitAddon = new FitAddon()
+    // this.terminal.loadAddon(fitAddon)
+    this.terminal.open(document.getElementById('terminal'), true)
+    this.webSocket = new WebSocket('ws://127.0.0.1:8000/webshell')
+    this.webSocket.onopen = this.socketConnected
+    this.webSocket.onclose = this.socketClosed
+    this.webSocket.onerror = this.socketError
+    this.webSocket.onmessage = this.socketMessage
+    this.terminal.loadAddon(new AttachAddon(this.webSocket))
+    // fitAddon.fit()
   }
 }
 </script>
